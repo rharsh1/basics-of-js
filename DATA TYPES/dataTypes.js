d9110cc8ae53d7df37dd56ec932a2c5a2e82da8d
harsh <rharshrajput7056@gmail.com>
2021-08-11 15:38:26 -0700

    data type error silve

---
+++ DATA TYPES/dataTypes.js
@@ -0,0 +1,19 @@
+// Numbers
+
+var x = 12;
+var y = 13;
+sum = x + y;
+const  arr=[1,12,35,5,3,6];
+add = 0;
+for(i=0;i<arr.length;i++){
+    add += arr[i];
+}
+console.log(sum)
+console.log(add)
+
+// string
+
+let str = rohit;
+let str1 = sharma;
+str2 = str + str1
+console.log(str2)
